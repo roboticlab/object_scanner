ec76f765d1c99a243409606deae20cf7883b64a6
svyatoslavdm <svyatoslavdm@gmail.com>
2016-07-01 11:47:07 +0300

    added main function

---
+++ src/object_scanner.cpp
@@ -0,0 +1,15 @@
+#include <sstream>
+#include <ros/ros.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "object_scanner");
+    ros::NodeHandle n;
+
+    while (ros::ok())
+    {
+
+    }
+
+    return 0;
+}
