b5c73d54ae17bbdd3bee7723a765a31413622d37
svyatoslavdm <svyatoslavdm@gmail.com>
2016-07-04 11:28:27 +0300

    prepared to master branch

--- src/cloud_processor.cpp
+++ src/cloud_processor.cpp
@@ -38,44 +38,44 @@ void CloudProcessor::tranformCloud(pcl::PointCloud< pcl::PointXYZRGB >::Ptr inpu
 void CloudProcessor::passThroughFilter(pcl::PointCloud< pcl::PointXYZRGB >::Ptr input, pcl::PointCloud< pcl::PointXYZRGB >::Ptr output, 
 				       std::string field, float nx, float px, bool keep_organized = true)
 {
-    pcl::PassThrough<pcl::PointXYZRGB> pass;
-    pass.setKeepOrganized(keep_organized);
-    pass.setInputCloud (input);
-    pass.setFilterFieldName (field);
-    pass.setFilterLimits (nx, px);
-    pass.filter (*output);  
+//     pcl::PassThrough<pcl::PointXYZRGB> pass;
+//     pass.setKeepOrganized(keep_organized);
+//     pass.setInputCloud (input);
+//     pass.setFilterFieldName (field);
+//     pass.setFilterLimits (nx, px);
+//     pass.filter (*output);  
 }
 void CloudProcessor::bilateralFilter(pcl::PointCloud< pcl::PointXYZRGB >::Ptr input, pcl::PointCloud< pcl::PointXYZRGB >::Ptr output, float sigma_r, float sigma_s)
 {
-    pcl::FastBilateralFilterOMP<pcl::PointXYZRGB> fbFilter;
-    fbFilter.setInputCloud(input); 
-    fbFilter.setSigmaR(sigma_r);    
-    fbFilter.setSigmaS(sigma_s);  
-    fbFilter.applyFilter(*output);   
+//     pcl::FastBilateralFilterOMP<pcl::PointXYZRGB> fbFilter;
+//     fbFilter.setInputCloud(input); 
+//     fbFilter.setSigmaR(sigma_r);    
+//     fbFilter.setSigmaS(sigma_s);  
+//     fbFilter.applyFilter(*output);   
 }
 void CloudProcessor::icpAlighn(pcl::PointCloud< pcl::PointXYZRGB >::Ptr _source, pcl::PointCloud< pcl::PointXYZRGB >::Ptr _target, pcl::PointCloud< pcl::PointXYZRGB >::Ptr _cloud_out)
 {
-    pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> ICPAlighner;
-    ICPAlighner.setInputSource(_source); //model
-    ICPAlighner.setInputTarget (_target); //scene
-    ICPAlighner.setMaximumIterations (500);
-    ICPAlighner.setMaxCorrespondenceDistance (0.01);
-    ICPAlighner.setTransformationEpsilon (1e-6);
-    ICPAlighner.align (*_cloud_out);
+//     pcl::IterativeClosestPoint<pcl::PointXYZRGB, pcl::PointXYZRGB> ICPAlighner;
+//     ICPAlighner.setInputSource(_source); //model
+//     ICPAlighner.setInputTarget (_target); //scene
+//     ICPAlighner.setMaximumIterations (500);
+//     ICPAlighner.setMaxCorrespondenceDistance (0.01);
+//     ICPAlighner.setTransformationEpsilon (1e-6);
+//     ICPAlighner.align (*_cloud_out);
 }
 
 // ---------------- Constructor, callbacks, common functions
 void CloudProcessor::pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& pc_msg)
 {
-    input_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>());
-    pcl::fromROSMsg(*pc_msg, *this->input_cloud);
+//     input_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>());
+//     pcl::fromROSMsg(*pc_msg, *this->input_cloud);
 }
 CloudProcessor::CloudProcessor()
 {
-    ROS_INFO_STREAM("CloudProcessor class created");
-    
-    cloud_subscriber =  nh_.subscribe(subs_cloud_topic.c_str(),1,&CloudProcessor::pointCloudCallback, this);
-    
+//     ROS_INFO_STREAM("CloudProcessor class created");
+//     
+//     cloud_subscriber =  nh_.subscribe(subs_cloud_topic.c_str(),1,&CloudProcessor::pointCloudCallback, this);
+//     
     input_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>());
     temp_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>());
     temp2_cloud.reset(new pcl::PointCloud<pcl::PointXYZRGB>());
